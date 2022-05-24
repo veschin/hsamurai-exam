85d1e1d439262cb7b48b98ef94b473894c6c361a
Oleg Veschin <veschindev@gmail.com>
2022-05-25 01:55:24 +0700

    removed useless test. add resource sharing.

--- src/back/main.clj
+++ src/back/main.clj
@@ -1,15 +1,13 @@
 (ns back.main
   (:require
    [route-map.core :as rm]
-   [ring.util.response :refer [response not-found]]
+   [ring.util.response :refer [file-response not-found]]
    [ring.adapter.jetty]
-   [ring.middleware.multipart-params
-    :refer [wrap-multipart-params]]
-
+   [ring.middleware.resource :refer [wrap-resource]]
    [clojure.test :as t]))
 
 (def routes
-  {:GET (constantly (ring.util.response/file-response "resources/index.html"))})
+  {:GET (constantly (file-response "resources/index.html"))})
 
 (defn dispatcher [{:keys [uri request-method] :as req}]
   (prn uri)
@@ -17,30 +15,23 @@
     (match req)
     (not-found "Path not found")))
 
-(t/deftest valid-dispatch?
-  (t/is
-   (= (dispatcher
-       {:uri            "/"
-        :request-method :get})
-      {:status  200
-       :headers {}
-       :body    123})))
-
 (def server
   (ring.adapter.jetty/run-jetty
-   dispatcher
+   (wrap-resource dispatcher "public")
    {:port  8080
     :join? false}))
 
 (comment
-  (valid-dispatch?)
 
   (.start server)
   (.stop server)
-  (require)
+
+  (require 'ring.middleware.resource)
   ring.middleware.resource/wrap-resource
-  (slurp (:body ))
+
+  (slurp (:body))
   (slurp "http://localhost:8080")
 
+  (rm/match [:get "/js"] routes)
   ;;
   )
