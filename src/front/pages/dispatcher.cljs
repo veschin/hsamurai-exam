c651e2cb9fe7fb352ceb6392e58dbb546c2b914e
Oleg Veschin <veschindev@gmail.com>
2022-05-30 10:26:03 +0700

    match-route now pass args into matched func, when they exists.

--- src/front/pages/dispatcher.cljs
+++ src/front/pages/dispatcher.cljs
@@ -1,20 +1,24 @@
 (ns front.pages.dispatcher
   (:require [route-map.core :as mc]
             front.pages.list
-            front.pages.create))
+            front.pages.create
+            front.pages.edit))
 
 (def routes
   {"patients"
-   {"list" {:. front.pages.list/view}
+   {"list"   {:. front.pages.list/view}
     "create" {:. front.pages.create/view}
-    }})
+    "edit"   {[:id] front.pages.edit/view}}})
 
 (defn match-route [path]
-  (or (:match (mc/match path routes))
-      [:div "Impossible but, page not found!"]))
+  (let [{:keys [match params]} (mc/match path routes)]
+    (or (if (not-empty params) (match params) match)
+        [:div "Impossible but, page not found!"])))
 
 (comment
   (match-route "/")
   (:match (mc/match "/patients/list" routes))
+  (not-empty {} )
+  (:params (mc/match "/patients/edit/1" routes))
   ;;
   )
