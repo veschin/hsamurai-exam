99089142fca160e3f3fe0f6264c03b0ff20e1ce4
Oleg Veschin <veschindev@gmail.com>
2022-06-02 17:51:10 +0700

    created edn file for fill db on start. added simple test for patients route.

--- src/back/main.clj
+++ src/back/main.clj
@@ -1,19 +1,19 @@
 (ns back.main
   (:gen-class)
   (:require
-   [route-map.core :as rm]
    [ring.util.response :refer [file-response not-found]]
    [ring.adapter.jetty]
    [ring.middleware.resource :refer [wrap-resource]]
    [ring.middleware.params :refer [wrap-params]]
    [ring.middleware.json :refer [wrap-json-params]]
    [ring.middleware.keyword-params :refer [wrap-keyword-params]]
-   back.api
+
+   [route-map.core :as rm]
    [clojure.test :as t]
-   
+
+   [back.api]
    [back.db.dsl :as dsl]
-   [back.db.schema :refer [schema]]
-   ))
+   [back.db.schema :refer [schema]]))
 
 (def routes
   {"patients" {[:view] {:GET (constantly (file-response "resources/index.html"))
@@ -37,25 +37,36 @@
      {:port  8080
       :join? false})))
 
-(defn create-tables []
+(defn create-and-fill-tables []
   (let [{patient-fields :patients} schema]
-      ;; (dsl/exec! {:drop-table-if-exists :patients})
-      (dsl/exec! {:create-table-if-not-exists
-                  [:!!patients patient-fields]})))
-
-;; (defn fill-by-random []
-  ;; {:})
+    (dsl/exec! {:drop-table-if-exists :patients})
+    (dsl/exec! {:create-table-if-not-exists
+                [:!!patients patient-fields]})
+    (try
+      (do
+        (require 'clojure.tools.reader)
+        (doseq [patient (clojure.tools.reader/read-string (slurp "patients.edn"))]
+          (dsl/insert! :patients patient)))
+      (catch Exception e (str "Edn file corrupted or doesn`t exist\n error->" e)))))
 
 (defn -main [& _]
-  (create-tables)
+  (create-and-fill-tables)
   (server))
 
+(t/deftest valid-patients-routes?
+    (let [request-map (fn [uri] {:request-method :get :uri uri})
+          test-get #(-> % request-map dispatcher :status #{200} boolean)]
+      (t/is (test-get "/patients/list"))
+      (t/is (test-get "/patients/create"))
+      (t/is (test-get "/patients/edit"))
+      (t/is (test-get "/patients/any-other-page"))
+      (t/is (not (test-get "/patients")))
+      (t/is (not (test-get "/")))))
+
 (comment
-  
   (.start jetty-server)
   (.stop jetty-server)
-  (dsl/exec! {:drop-table-if-exists :patients})
   
-  (:match (rm/match [:get "/patients/list"] routes))
+  (valid-patients-routes?)
   ;;
   )
